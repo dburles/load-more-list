bbc15474169b949dc5984eedad2222b0706d494f
David Burles <dburles@gmail.com>
2015-01-30 20:01:12 +1100

    update meteor + add comments

--- server/publications.js
+++ server/publications.js
@@ -2,6 +2,7 @@ Meteor.publish('messagesList', function(limit) {
   check(limit, Number);
   Counts.publish(this, 'messagesList', Messages.list(), { noReady: true });
 
+  // mimic latency
   Meteor._sleepForMs(1000);
 
   return Messages.list({ limit: limit });
@@ -11,6 +12,7 @@ Meteor.publish('booksList', function(limit) {
   check(limit, Number);
   Counts.publish(this, 'booksList', Books.list(), { noReady: true });
 
+  // mimic latency
   Meteor._sleepForMs(1000);
 
   return Books.list({ limit: limit });
